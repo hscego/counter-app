279d281b2f5a887260704ffc40306803317bcaaf
Hannah Scego <53790113+hscego@users.noreply.github.com>
2025-03-11 11:17:00 -0700

    Update counter.py
    adding in function

--- counter.py
+++ counter.py
@@ -1,2 +1,10 @@
 // counter python back end
-import numpy as np
+import time
+
+def simple_timer(seconds):
+    print(f"Timer started for {seconds} seconds.")
+    time.sleep(seconds)
+    print("Time's up!")
+
+#Set a timer for 5 seconds
+simple_timer(5.5)
